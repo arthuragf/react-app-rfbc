185bdc999ff5ad38965f8ad11e43e79306c28059
arthur.agf <arthur.agf@gmail.com>
2025-06-22 23:46:29 -0300

    Add Brave Search to the list

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,12 @@ function App() {
         description="A privacy-focused search engine that doesn't track you. Simple and effective search results."
         url="https://duckduckgo.com"
       />
+      <Card
+        title="Brave"
+        img_src="https://play-lh.googleusercontent.com/aP7LHNpjMJJxoR8jRPPdVPlISDNNkFBKKmdODDp0uRNlL3YTugEdNEcHJgKBeRV8eeuU"
+        description="An independent search engine that puts privacy first and avoids relying on Big Tech."
+        url="https://search.brave.com"
+      />
     </>
 )
 }
