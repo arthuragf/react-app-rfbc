71aaddbf4783acf37350c4a1407281597e33a89c
arthur.agf <arthur.agf@gmail.com>
2025-06-24 17:36:48 -0300

    Rendering a unordered list from an array

---
+++ src/components/List.jsx
@@ -0,0 +1,8 @@
+const List = () => {
+    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"]
+    const listItems = fruits.map(fruit => <li>{fruit}</li>)
+    return (
+        <ul>{listItems}</ul>
+    )
+}
+export default List
