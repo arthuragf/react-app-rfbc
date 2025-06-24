c5df259dae9e094cf8916617789c3ff56bdb7666
arthur.agf <arthur.agf@gmail.com>
2025-06-23 23:51:03 -0300

    Conditional rendering

---
+++ src/components/UserGreeting.jsx
@@ -0,0 +1,7 @@
+const UserGreeting = (props) => {
+    if (props.isLoggedIn) {
+        return <h2>Welcome {props.userName}</h2>
+    }
+        return <h2>Please, log in to continue</h2>
+}
+export default UserGreeting
