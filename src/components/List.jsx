6abc9facf6bf83fa51853acd03d51671e22fb35b
arthur.agf <arthur.agf@gmail.com>
2025-06-24 17:44:09 -0300

    Rendering an ordered list from an array of objects

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,19 @@
 const List = () => {
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"]
-    const listItems = fruits.map(fruit => <li>{fruit}</li>)
-    return (
-        <ul>{listItems}</ul>
+    const fruits = [
+        {id: 1, name: "apple", calories: 95}, 
+        {id: 2, name: "orange", calories: 45}, 
+        {id: 3, name: "banana", calories: 105},
+        {id: 4, name: "coconut", calories: 159}, 
+        {id: 5, name: "pineapple", calories: 37}
+    ].sort((a,b) => b.calories - a.calories)
+    
+    const listItems = fruits.map(fruit => 
+        <li key={fruit.id}>
+            {fruit.name}: &nbsp;
+            <b>{fruit.calories}</b>
+        </li>
     )
+
+    return <ol>{listItems}</ol>
 }
 export default List
